9746495fed87ceb1c65b0c60ef496b1aef1aa60f
Ting <90290336+bostonrider@users.noreply.github.com>
2021-11-16 19:51:43 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -15,6 +15,8 @@
 
   plotset<-subset(powercom, as.Date("2007-02-01") <= Date & Date <=as.Date("2007-02-02"))
 
+  library("lubridate")
+
 #The first plot
 
   hist(plotset$Global_active_power, col="red", xlab="Global Acitve Power (kilowatts)",
